1041884293f448c2cf50bc72c31946795b603bc0
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-08 08:26:26 +0000

    [Update] Update機能の作成途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id), method: :get
+  end
+
   private
   def book_params
     params.permit(:title, :body)
