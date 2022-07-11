c281212a6b8064b34e2d1a928b9bab8539c0e095
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-11 01:45:33 +0000

    [Update] バリデーションの実装(update失敗時)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,12 +28,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
-      redirect_to book_path(book.id), method: :get
+      redirect_to book_path(@book.id), method: :get
     else
-      render :show
+      render :edit
     end
   end
 
